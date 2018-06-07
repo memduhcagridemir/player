df66f69ebef9e08fc46eaea5bc3c4ff8101556cc
memduhcagridemir <memduhcagridemir@gmail.com>
2018-06-08 00:24:41 +0300

    Playlist POST endpoint added.

--- player/src/AppBundle/Controller/PlaylistRestController.php
+++ player/src/AppBundle/Controller/PlaylistRestController.php
@@ -4,56 +4,85 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Playlist;
 use AppBundle\Form\PlaylistType;
+use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as FOSRest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use FOS\RestBundle\View\View;
+use AppBundle\Exception\InvalidFormException;
 
 /**
- * Audio controller.
+ * Playlist REST controller.
  *
  * @Route("/api/playlist")
  */
 class PlaylistRestController extends FOSRestController
 {
     /**
-     * @FOSRest\Get("/playlist")
+     * @FOSRest\Get("/", name="rest_playlist_list")
      */
     public function listAction()
     {
-        $restresult = $this->getDoctrine()->getRepository('AppBundle:Playlist')->findAll();
-        if ($restresult === null) {
+        $playlists = $this->getDoctrine()->getRepository('AppBundle:Playlist')->findAll();
+        if ($playlists === null) {
             return new View("there are no playlists exist", Response::HTTP_NOT_FOUND);
         }
-        return $restresult;
+        return $playlists;
     }
 
     /**
-     * Creates a new playlist entity.
-     *
-     * @Route("/new", name="playlist_new")
-     * @Method({"GET", "POST"})
+     * @FOSRest\Get("/{id}", name="rest_playlist_get")
      */
-    public function newAction(Request $request)
+    public function getAction(int $id)
     {
-        $playlist = new Playlist();
-        $playlist->setUser($this->getUser());
-        $form = $this->createForm(PlaylistType::class, $playlist);
-        $form->handleRequest($request);
+        $playlist = $this->getDoctrine()->getRepository('AppBundle:Playlist')->findOneBy(["id" => $id]);
+        if ($playlist === null) {
+            return new View("there are no playlists exist", Response::HTTP_NOT_FOUND);
+        }
+        return $playlist;
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
+    private function processForm(Playlist $playlist, array $data, $method = 'PUT') {
+        $form = $this->createForm("AppBundle\Form\PlaylistType", $playlist, ['method' => $method, 'csrf_protection' => false]);
+
+        $form->submit($data, 'PATCH' !== $method);
+
+        if ($form->isValid()) {
+            /** @var Playlist $playlist */
+            $playlist = $form->getData();
+
+            $user = $this->getDoctrine()->getRepository('AppBundle:User')->findOneBy(['id' => 9]);
+            $playlist->setUser($user); // TODO
+
+            /** @var \Doctrine\Common\Persistence\ObjectManager $em */
             $em = $this->getDoctrine()->getManager();
             $em->persist($playlist);
             $em->flush();
 
-            return $this->redirectToRoute('manage_index');
+            return $playlist;
         }
 
-        return $this->render(':playlist:new.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        throw new InvalidFormException($form);
+    }
+
+    /**
+     * @FOSRest\Post("/", name="rest_playlist_new")
+     */
+    public function newAction(Request $request)
+    {
+        try {
+            $playlist = $this->processForm(new Playlist(), $request->request->all(), 'POST');
+
+            $routeOptions = [ 'id' => $playlist->getId(), '_format' => $request->get('_format') ];
+            return $this->routeRedirectView('rest_playlist_get', $routeOptions, Response::HTTP_CREATED);
+        }
+        catch (InvalidFormException $exception) {
+            return $exception->getForm();
+        }
     }
 
     /**
